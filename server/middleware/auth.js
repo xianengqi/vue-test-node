b6793808b558cc0b9429c389e118be5a81d1e26b
xianengqi <920986617@qq.com>
2020-01-25 20:54:24 +0800

    middleware
    后端用户登录中间件

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,9 +1,13 @@
 // 写成一个函数,想配置就配置,想调用就调用
 module.exports = options => {
+  const jwt = require('jsonwebtoken')
+  const AdminUser = require('../models/AdminUser')
+  const assert = require('http-assert')
+
   return async (req, res, next) => {
     const token = String(req.headers.authorization || '').split(' ').pop()
     assert(token, 401, '请提供jwt token')
-    const { id } = jwt.verify(token, app.get('secret'))
+    const { id } = jwt.verify(token, req.app.get('secret'))
     assert(id, 401, '无效的jwt token')
     req.user = await AdminUser.findById(id)
     assert(req.user, 401, '请先登录')
